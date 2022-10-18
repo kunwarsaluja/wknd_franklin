a391e818ab2bfdf59d852e59bb119b91955eb93d
Kunwar <saluja@adobe.com>
2022-10-17 21:22:54 -0400

    eslint fix

--- blocks/faqs/faqs.js
+++ blocks/faqs/faqs.js
@@ -1,55 +1,48 @@
 export default function decorate(block) {
-    const faq_rows = Array.from(block.children);
-    const faqs=[];
-    //console.log(faq_rows);
+  const faqRows = Array.from(block.children);
+  const faqs = [];
+  // console.log(faq_rows);
 
-  faq_rows.forEach((row)=>{
-    const faq_cell=row.innerText.split('\n');
-    const faq_question=faq_cell[1];
-    const faq_answer=faq_cell[2];
-    faqs.push({faq_question,faq_answer});
+  faqRows.forEach((row) => {
+    const faqCell = row.innerText.split('\n');
+    const faqQuestion = faqCell[1];
+    const faqAnswer = faqCell[2];
+    faqs.push({ faqQuestion, faqAnswer });
+  });
 
+  block.innerHTML = '';
+  faqs.forEach((faq) => {
+    const { faqQuestion, faqAnswer } = faq;
+
+    const accordion = document.createElement('div');
+    accordion.className = 'faq-accordion';
+    block.append(accordion);
+
+    const questionDiv = document.createElement('button');
+    questionDiv.className = 'faq-question';
+    accordion.append(questionDiv);
+    questionDiv.innerHTML = faqQuestion;
+
+    const answerDiv = document.createElement('div');
+    answerDiv.className = 'faq-answer';
+    accordion.append(answerDiv);
+    answerDiv.innerHTML = faqAnswer;
+  });
+
+  const acc = document.getElementsByClassName('faq-question');
+  let i;
+
+  // eslint-disable-next-line no-plusplus
+  for (i = 0; i < acc.length; i++) {
+    // eslint-disable-next-line func-names
+    acc[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+      const faqAnswer = this.nextElementSibling;
+      if (faqAnswer.style.display === 'block') {
+        faqAnswer.style.display = 'none';
+      } else {
+        faqAnswer.style.display = 'block';
+      }
     });
-
-    block.innerHTML = '';
-    faqs.forEach((faq)=>{
-        const { faq_question, faq_answer } = faq;
-
-        const accordion = document.createElement('div');
-        accordion.className = 'faq-accordion';
-        block.append(accordion);
-    
-        const questionDiv = document.createElement('button');
-        questionDiv.className = 'faq-question';
-        accordion.append(questionDiv);
-        questionDiv.innerHTML = faq_question;
-    
-    
-        const answerDiv = document.createElement('div');
-        answerDiv.className = 'faq-answer';
-        accordion.append(answerDiv);
-        answerDiv.innerHTML = faq_answer;
-
-
-    });
-    
-    var acc = document.getElementsByClassName("faq-question");
-    var i;
-    
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      });
-    }     
   }
-
-
-
-
-
+}
