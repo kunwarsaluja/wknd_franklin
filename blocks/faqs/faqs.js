096e66f22a17c6b0464bdcf0d85f41880713de7e
Brian Warner <3vil3mpir3@gmail.com>
2022-10-19 00:09:54 -0400

    added documentation comments to functions

--- blocks/faqs/faqs.js
+++ blocks/faqs/faqs.js
@@ -21,8 +21,18 @@ export default function decorate(block) {
   bindClick(acc);
 }
 
+/**
+  * Constructs dom elements for this block. Called from the decorate funciton.
+  * Returns an element if doinner is FALSE.
+  * 
+  * @param {element} el element to be constructed
+  * @param {string} classname class name to be added to the element
+  * @param {element} appendtarget dom element target this element will be appended to
+  * @param {boolean} doinner determins if the element has innerHtml or not.  
+  * @param {html} doinner HTML to append in the created element
+ */
 function domConstructor(el,classname,appendtarget,doinner,innerhtml){
-  var thiselement = document.createElement(el);
+  const thiselement = document.createElement(el);
   thiselement.className = classname;
   appendtarget.append(thiselement);
   if(doinner){
@@ -32,6 +42,11 @@ function domConstructor(el,classname,appendtarget,doinner,innerhtml){
   }
 }
 
+/**
+  * Binds the click action, calls addClass and clearClass functions.  Called from the decorate function.
+  * 
+  * @param {element} acc element array 
+ */
 function bindClick(acc){
   for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener('click', function () {
@@ -41,11 +56,23 @@ function bindClick(acc){
   }
 }
 
+/**
+  * Adds classes to show the clicked element, called from bindClick
+  * 
+  * @param {element} acc element array 
+  * @param {string} classname class name to be added to the element 
+ */
 function addClass(acc,classname){
   acc.classList.add(classname);
   acc.nextElementSibling.classList.add(classname);
 }
 
+/**
+  * removes classes to hide the clicked element, called from bindClick
+  * 
+  * @param {element} acc element array 
+  * @param {string} classname class name to be added to the element 
+ */
 function clearClass(acc,classname){
   [].forEach.call(acc, function(el) {
     el.classList.remove(classname);
