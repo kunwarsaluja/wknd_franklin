a03e576c384f1342b58c408e5309a6313096f851
Kunwar <saluja@adobe.com>
2022-10-26 15:26:49 -0400

    gdrive embed

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -1,8 +1,8 @@
 export default function decorate(block) {
   const url = Array.from(block.children);
-  const embedUrl = url[0].innerText.split('/')[4];
+  const embedUrl = url[0].innerText.split('/')[5];
 
-  const embedHTML = `<iframe src="https://web.microsoftstream.com/embed/video/${embedUrl}" 
+  const embedHTML = `<iframe src="https://drive.google.com/file/d/${embedUrl}/preview" 
       style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" 
       frameborder="0" allow="autoplay; fullscreen; picture-in-picture"  
       title="Content from MS Stream" loading="lazy"></iframe>`;
